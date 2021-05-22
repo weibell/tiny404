fa9dc5100362c4a33c92f6716fc55b2734c367d0
weibell <13382354+weibell@users.noreply.github.com>
2021-05-22 17:41:50 +0200

    Fix log output

--- start.sh
+++ start.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+log_request() {
+  COUNTER=$((COUNTER+1))
+  echo "[$(date -u +%FT%TZ)] Request #${COUNTER}"
+}
+
 case $STATUS_CODE in
   200) REASON="OK";;
   401) REASON="Unauthorized";;
@@ -12,8 +17,7 @@ esac
 STATUS_LINE="HTTP/1.1 ${STATUS_CODE} ${REASON}\r\n"
 HEADER_FIELDS="Connection: close\r\nContent-Length: 0\r\n\r\n"
 
-COUNTER=1
 while true ; do
   printf "${STATUS_LINE}${HEADER_FIELDS}" | ncat -l 80 --nodns --send-only
-  echo "[$(date -u +%FT%TZ)] Request #${COUNTER}" && COUNTER=$((COUNTER+1))
+  log_request
 done
